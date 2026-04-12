3e163f5235c1a027ab89e5d65025475cbe232849
joaovitor120 <jvbindacaetano.dev@gmail.com>
2026-04-12 16:04:55 -0300

    devoloping the update function

--- database/adm_manage.py
+++ database/adm_manage.py
@@ -8,7 +8,6 @@ options_available = ['1', '2']
 databases_availables = ("1 - Calculation informations \n2 - User Informations")
 input_label = ("Please type 1/2: ")
 
-
 def input_verified(user_input, options_available, input_label):
     while user_input not in options_available: #only accepted 1/2 options
         print(f"Please, type a valid option.")
@@ -22,19 +21,20 @@ def get_db_table_input():
 
 def get_table_columns(table_name):
     columns = []
-    cursor.execute(f"PRAGMA table_info('CalcInfos')") #see CalcInfos informations
+    cursor.execute(f"PRAGMA table_info('{table_name}')") #see CalcInfos informations
     table_info = cursor.fetchall()
     for column_info in table_info:
         column = column_info[1]
         columns.append(column)
-    return columns
-
+    return columns #return a dict 
+    
 def see_datas(): #see datas on the database
     db = get_db_table_input()
     cursor.execute(f"SELECT * FROM {tables[db - 1]}")
     datas = cursor.fetchall() #touple
     for i in datas:
         print(i)
+        
 def update_datas(): #update datas on the database
     print("These are the columns at CalcInfos:")
     columns = get_table_columns("CalcInfos")
@@ -43,17 +43,25 @@ def update_datas(): #update datas on the database
     for column in columns:
         print(f"{column}({i+1})")
         i+=1
-    input_label = "Which column do you want to update:"
-    column_selected_input = input(input_label)
-    column_selected = input_verified(column_selected_input, (columns + index_columns), input_label)
+    column_selected_label = "Which column do you want to update:"
+    column_selected_input = input(column_selected_label)
+    column_selected = input_verified(column_selected_input, (columns + index_columns), column_selected_label)
 
     if column_selected in index_columns:
         column_selected = columns[(int(column_selected) - 1)]
-    print(column_selected)
+    print("\n" + f"lines FROM column {column_selected}: ")
     cursor.execute(f"SELECT {column_selected} FROM CalcInfos")
     lines = cursor.fetchall()
+    i = 1
+    lines_qtd = []
     for line in lines:
-        print(line)
+        print(f"{i} - {line}")
+        lines_qtd.append(str(i))
+        i+=1
+    print("\n")
+    line_selected_label = (f"Type the line do you want to update {((str(lines_qtd)).replace("[", "(").replace("]", ")"))}: ")
+    line_selected = input(line_selected_label)
+    line_selected = input_verified(line_selected, lines_qtd, line_selected_label)
     #continue here implementing the update function
     
 def delete_datas(): #delete datas on the database
